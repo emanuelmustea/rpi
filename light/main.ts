32f73972f92af1c0118ed1e13753380c031ad888
Emanuel Mustea <plus.emanuelmustea@gmail.com>
2024-11-07 01:14:39 +0200

    latyest

--- light/main.ts
+++ light/main.ts
@@ -2,7 +2,7 @@ import { executeInstructions, Pin, PinDirection } from "https://deno.land/x/deno
 
 // alternative hybrid (transfer from bash to JS and vice-versa)
 // note that this approach is a bit slower and can cause timing issues with some 'dumb' sensors that require precise instruction sequences.
-const led = new Pin(24, PinDirection.OUT, 1);
+const led = new Pin(6, PinDirection.OUT, 1);
 // the Pin constructor queues up some export instructions.. we are executing these here
 await executeInstructions(); // calls sysfs. returns a promise.
 
